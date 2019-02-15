4297fb63e786a050436b38f402306f864591f733
XGHeaven <xgheaven@gmail.com>
2019-02-15 15:25:18 +0800

    feat: add proxy dns lookup

--- src/service/download.service.ts
+++ src/service/download.service.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
+import { lookup } from 'dns'
 import { ConfigService } from 'nestjs-config'
 import { Agent } from 'http'
 import fetch from 'node-fetch'
+import { parse } from 'url'
 
 import ProxyAgent = require('proxy-agent')
 
 @Injectable()
-export class DownloadService {
+export class DownloadService implements OnModuleInit {
   private agent?: Agent
-  constructor(private config: ConfigService) {
-    const proxy = config.get('download.proxy')
-    if (proxy) {
-      this.agent = new ProxyAgent(proxy)
+  private logger: Logger = new Logger('DownloadService', false)
+  constructor(private config: ConfigService) { }
+
+  async onModuleInit() {
+    const proxyString = this.config.get('download.proxy')
+    if (proxyString) {
+      const uri = parse(proxyString)
+      // 自己解析 ip 地址
+      await new Promise((resolve, reject) => {
+        lookup(uri.hostname!, (e, ip) => {
+          if (e) {
+            this.logger.warn('Found proxy config but cannot lookup dns, so disabled.')
+            return
+          }
+
+          uri.hostname = ip
+        })
+
+        this.agent = new ProxyAgent(uri)
+      })
+
     }
   }
 
--- src/typings/proxy-agent/index.d.ts
+++ src/typings/proxy-agent/index.d.ts
@@ -2,7 +2,7 @@ declare module 'proxy-agent' {
   import { Agent } from 'http'
 
   class ProxyAgentConstructor extends Agent {
-    constructor(options: string)
+    constructor(options: string | object)
   }
 
   export = ProxyAgentConstructor
